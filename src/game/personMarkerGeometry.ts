49d33b8a095318221a1709ab8a6d9eb07407f113
Gabriel Weidmann <gabriel@weidmann.net>
2026-09-18 06:48:33 +0200

    Fix generated renderer newlines

--- src/game/personMarkerGeometry.ts
+++ src/game/personMarkerGeometry.ts
@@ -1,7 +1,8 @@
 import type { Person, World } from "../simulation/model";
 import { key } from "../simulation/hex";
 import { personWorldPosition } from "../simulation/movement";
-import { HEX_Y, pixel } from "./mapGeometry";\nimport { personInsideBuilding } from "./personVisibility";
+import { HEX_Y, pixel } from "./mapGeometry";
+import { personInsideBuilding } from "./personVisibility";
 
 export const PERSON_MARKER_RADIUS = HEX_Y / 2;
 export const PERSON_FEET_OFFSET_Y = HEX_Y * 0.15;
