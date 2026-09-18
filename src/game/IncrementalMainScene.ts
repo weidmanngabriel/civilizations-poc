e9ac05b83e055ecb03b861f178dd3aa890db5f6b
Gabriel Weidmann <gabriel@weidmann.net>
2026-09-18 18:40:53 +0200

    Render people above world resources

--- src/game/IncrementalMainScene.ts
+++ src/game/IncrementalMainScene.ts
@@ -97,8 +97,8 @@ export class IncrementalMainScene extends MainScene {
     this.inventoryGraphics = this.add.graphics();
     this.staffGraphics = this.add.graphics();
     this.inventoryLabels = this.add.container(0, 0);
-    this.personLayer = this.add.container(0, 0);
-    markers.add([this.bushGraphics, this.inventoryGraphics, this.staffGraphics, this.inventoryLabels, this.personLayer]);
+    this.personLayer = this.add.container(0, 0).setDepth(30);
+    markers.add([this.bushGraphics, this.inventoryGraphics, this.staffGraphics, this.inventoryLabels]);
     return true;
   }
 
