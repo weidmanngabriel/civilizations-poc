d0d2b67444d4aefbcc5dedbe8dfbdd3a72696fb9
Gabriel Weidmann <gabriel@weidmann.net>
2026-09-18 19:23:27 +0200

    Fix generated newlines in main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,8 @@ import { installLooseGoodsIndicators } from "./game/looseGoodsIndicators";
 import { installNaturalResourceIndicators } from "./game/naturalResourceIndicators";
 import { installPersonSelection } from "./game/personSelection";
 import { installSleepIndicators } from "./game/sleepIndicators";
-import { installWorkAreaInteraction } from "./game/workAreaInteraction";\nimport { installWaypostIndicators } from "./game/waypostIndicators";
+import { installWorkAreaInteraction } from "./game/workAreaInteraction";
+import { installWaypostIndicators } from "./game/waypostIndicators";
 import {
   installMobileMapTouchControls,
   preventMobilePageZoom,
@@ -110,7 +111,8 @@ installBuildingSprites(scene, world);
 installHungerIndicators(scene, world);
 installSleepIndicators(scene, world);
 installPersonSelection(scene, world);
-installWorkAreaInteraction(scene, world);\ninstallWaypostIndicators(scene, world);
+installWorkAreaInteraction(scene, world);
+installWaypostIndicators(scene, world);
 installTileSelectionGuard();
 mountControls(world, () => scene.renderWorld());
 installPerformanceDebugPanel(world);
