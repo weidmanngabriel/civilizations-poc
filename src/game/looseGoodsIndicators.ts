e70a5a57244ede6738dce0bcc1cfd7f8a6f729fd
Gabriel Weidmann <gabriel@weidmann.net>
2026-09-19 21:20:41 +0200

    Add shoes loose-good color

--- src/game/looseGoodsIndicators.ts
+++ src/game/looseGoodsIndicators.ts
@@ -13,6 +13,7 @@ const goodColors: Record<Good, number> = {
   fish: 0x6fa7b8,
   meat: 0xb85f55,
   leather: 0x8a5a3b,
+  shoes: 0x6f513b,
   clay: 0x9b6a4d,
   rubble: 0x8b8f8c,
   brick: 0xb55d42,
