448eecc9c4f0e0eb091295c29a405543760f618d
Gabriel Weidmann <gabriel@weidmann.net>
2026-09-19 15:41:15 +0200

    Spawn extensible hare groups

--- src/simulation/scenario.ts
+++ src/simulation/scenario.ts
@@ -20,6 +20,7 @@ import {
 } from "./spatial";
 import { hexDistance as fineHexDistance, key, tileIndex, walkable } from "./hex";
 import { ensureInitialWaypost } from "./wayposts";
+import { spawnAnimalGroup } from "./wildlife";
 import {
   naturalResourceBlocksMovement,
   naturalResourceFootprint,
@@ -469,6 +470,7 @@ function createScenario({ population, suppliedStart }: ScenarioOptions): World {
     ...(suppliedStart ? { unlockedTechnologies: [...STARTING_TECHNOLOGIES] } : {}),
     buildings,
     naturalResources,
+    ...(suppliedStart ? { animals: [], animalGroups: [], projectiles: [] } : {}),
     ...(suppliedStart ? { wayposts: [] } : {}),
     tiles,
     people,
@@ -481,6 +483,10 @@ function createScenario({ population, suppliedStart }: ScenarioOptions): World {
       const position = startPositions[index];
       if (position) person.position = { ...position };
     }
+
+    spawnAnimalGroup(world, "hare", scaledAt(10, 6), 4);
+    spawnAnimalGroup(world, "hare", scaledAt(24, 14), 4);
+    spawnAnimalGroup(world, "hare", scaledAt(33, 18), 4);
   }
   return attachNeeds(attachSleep(world));
 }
