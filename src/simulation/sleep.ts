1bf9449086cda31298c2b3448bfd2e69cf86d46f
Gabriel Weidmann <gabriel@weidmann.net>
2026-09-22 17:51:41 +0200

    Treat lessons as work for sleep

--- src/simulation/sleep.ts
+++ src/simulation/sleep.ts
@@ -30,7 +30,7 @@ const sleepValue = (person: Person): number => {
 
 const secondsPerSleepPoint = (person: Person): number => {
   if (person.trip?.picked) return 4;
-  if (person.progress > 0 || (person.farmTask && person.path.length === 0)) return 4;
+  if (person.educationTask?.active || person.progress > 0 || (person.farmTask && person.path.length === 0)) return 4;
   if (person.path.length > 0) return 8;
   return 16;
 };
