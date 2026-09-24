67ca7b55a7dc9943d0bfaa43c3f465b1aa47a43f
Gabriel Weidmann <gabriel@weidmann.net>
2026-09-24 15:36:55 +0200

    Show fish schools in water

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { installBuildPlacementHighlights } from "./game/buildPlacementHighlights
 import { installBuildingAttentionIndicators } from "./game/buildingAttentionIndicators";
 import { installBuildingSprites } from "./game/buildingSprites";
 import { installFishingIndicators } from "./game/fishingIndicators";
+import { installFishSchoolIndicators } from "./game/fishSchoolIndicators";
 import { installWildlifeIndicators } from "./game/wildlifeIndicators";
 import { installBushIndicators } from "./game/bushIndicators";
 import { installDesktopBuildPlacement } from "./game/desktopBuildPlacement";
@@ -120,6 +121,7 @@ installBuildPlacementHighlights(scene, world);
 installBuildingSprites(scene, world);
 installBuildingAttentionIndicators(scene, world);
 installFishingIndicators(scene, world);
+installFishSchoolIndicators(scene, world);
 installWildlifeIndicators(scene, world);
 installHungerIndicators(scene, world);
 installSleepIndicators(scene, world);
